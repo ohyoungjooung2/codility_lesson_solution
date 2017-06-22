fdc72d82641e5fd4398bd929a7af363bd576ccd3
ohyoungjooung2 <wnapdlf05@gmail.com>
2017-06-22 17:19:55 +0900

    Adding nasty solution for reference to Lesson3TapeEquilibrium.rb

--- Lesson3TapeEquilibrium.rb
+++ Lesson3TapeEquilibrium.rb
@@ -13,3 +13,42 @@ def sol(a)
    #return result no need to use "return"
    ra.min
 end
+
+
+#Nasty solution, correctness 100%, performance 0%^^;
+#def solution(a)
+#     z=[]
+#     a.each { |e| z << e }
+#    
+#    #Remove last element of array a
+#    a.pop
+#    size=a.size
+#    
+#    #Second element (sum) start
+#    z.shift
+#    z=z.reverse
+#    #p z
+#    
+#    second_a=[]
+#    second_sum=0
+#    for i in (0..size-1)
+#      z.each { |z| second_sum+=z }
+#      second_a<<second_sum
+#      second_sum=0
+#      z.pop
+#    end
+#    
+#    
+#    
+#    
+#    first_a=[]
+#    sum=0
+#    a.each { |e| sum+=e; first_a << sum }
+#    
+#    
+#    final_result_a=[]
+#    for i in (0..size-1)
+#       final_result_a << (first_a[i] - second_a[i]).abs
+#    end
+#    final_result_a.min
+#end
