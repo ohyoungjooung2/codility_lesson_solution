d3e38143bd031a3d88f224c4d8dfe38256045ca5
ohyoungjooung <wnapdlf05@gmail.com>
2020-02-04 19:52:46 +0900

    Update lesson3_tc_perm_missing.rb

--- lesson3_tc_perm_missing.rb
+++ lesson3_tc_perm_missing.rb
@@ -12,7 +12,7 @@ end
 #a=[1]
 #solution(a)
 
-#Python3
+#Python3(Good? model using python set)
 def solution(A):
       A.sort()
       #[2,3] or []
@@ -31,3 +31,23 @@ def solution(A):
       else:
                #[1,2] is the B and A.then..
          return max(A)+1
+               
+               
+   
+#Python3 slow one and [1,2],[1,2,3] like these.. are not missing case..in which below primitive algorithum is not correct..
+def solution(A):
+    #[2,3] or []
+    if len(A)==0 or A[0] != 1:
+        return 1
+    # [1]
+    if len(A)==1 and A[0] == 1:
+        return (A[0]+1)
+    # write your code in Python 3.6
+    A.sort()
+    for i in range(0,len(A)):
+        if A[i] == max(A):
+            break
+        if A[i+1] > A[i]+1:
+            r = A[i] + 1
+            break
+    return r
