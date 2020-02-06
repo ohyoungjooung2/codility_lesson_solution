91ef7325a0c0f66a090238fb6d1491a3c66cdaa6
ohyoungjooung <wnapdlf05@gmail.com>
2020-02-06 14:02:48 +0900

    Update Lesson6Triangle.rb

--- Lesson6Triangle.rb
+++ Lesson6Triangle.rb
@@ -15,3 +15,22 @@ def solution(a)
    0
 
 end
+
+
+#Python3
+def solution(a):
+    a=sorted(a)
+    n=[]
+    for i in range(len(a)-2):
+        first=a[i]
+        second=a[i+1]
+        third=a[i+2]
+        n.append([first,second,third])
+    #print(n)
+    #print(n[0])
+    for i in range(len(n)):
+        #print(n[i][0],n[i][1],n[i][2])
+        if (n[i][0]+n[i][1] > n[i][2]) and (n[i][1]+n[i][2]>n[i][0]) and (n[i][0]+n[i][2]>n[i][1]):
+            return 1
+       
+    return 0
